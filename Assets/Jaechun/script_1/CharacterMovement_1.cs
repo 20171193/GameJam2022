d72bad69bae35dab8a45604911205af9991bc210
Shim Jae Chun <shimnaldo98@gmail.com>
2022-11-05 18:02:50 +0900

    종료전 push

--- Assets/Jaechun/script_1/CharacterMovement_1.cs
+++ Assets/Jaechun/script_1/CharacterMovement_1.cs
@@ -4,6 +4,18 @@ using UnityEngine;
 
 public class CharacterMovement_1 : MonoBehaviour
 {
+    [SerializeField] GameObject main_Background; //캐릭터가 위치하는 배경
+    [SerializeField] GameObject[] other_Background;  //메인 배경을 기준으로 둘러싸는 배경
+
+    private int upCheck = 0;
+    private int downCheck = 0;
+
+    public float rightTileToTile; //오른쪽 타일 간격
+    public float updownTileToTile; //위아래 타일 간격
+
+    private float bgWidth;   //배경 스프라이트의 너비값
+    private float bgHeight;  //배경 스프라이트의 높이값
+
     private Transform tr;
     private Rigidbody2D rd;
 
@@ -34,7 +46,12 @@ public class CharacterMovement_1 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        main_Background.transform.position = new Vector3(0, 0, 0);
+
+        bgWidth = main_Background.GetComponent<SpriteRenderer>().bounds.size.x;
+        bgHeight = main_Background.GetComponent<SpriteRenderer>().bounds.size.y;
+
+        OtherBackgroundSetting();
     }
 
     // Update is called once per frame
@@ -74,6 +91,11 @@ public class CharacterMovement_1 : MonoBehaviour
                 jumpable = false;
                 cur_xpos += wallManager_st.wallinterval_x;
                 cur_ypos += wallManager_st.wallinterval_y;
+
+                upCheck++; //배경
+                downCheck--;  //배경
+                MainBackgroundUp();  //배경
+                MainBackgroundForward();  //배경
             }
             if (Input.GetKeyUp(KeyCode.RightArrow)  // 이동 - 중
                 && jumpArrow == 1)
@@ -85,6 +107,8 @@ public class CharacterMovement_1 : MonoBehaviour
                 wallManager_st.CheckInPlayer();
                 jumpable = false;
                 cur_xpos += wallManager_st.wallinterval_x;
+
+                MainBackgroundForward(); //배경
             }
             if (Input.GetKeyUp(KeyCode.DownArrow) // 이동 - 하
                 && jumpArrow == 2)
@@ -97,7 +121,58 @@ public class CharacterMovement_1 : MonoBehaviour
                 jumpable = false;
                 cur_xpos += wallManager_st.wallinterval_x;
                 cur_ypos -= wallManager_st.wallinterval_y;
+
+                downCheck++; //배경
+                upCheck--; //배경
+                MainBackgroundDown(); //배경
+                MainBackgroundForward(); //배경
             }
         }
+    }
+
+
+    public void MainBackgroundForward()
+    {
+        if (transform.position.x % bgWidth < rightTileToTile)
+        {
+            main_Background.transform.position = new Vector3(main_Background.transform.position.x + bgWidth, main_Background.transform.position.y, 0);
+            OtherBackgroundSetting();
+        }
+    }
+
+    public void MainBackgroundUp()
+    {
+        if (upCheck >= (bgHeight / updownTileToTile) - 1)
+        {
+            if (Mathf.Abs(transform.position.y) % bgHeight < updownTileToTile)
+            {
+                main_Background.transform.position = new Vector3(main_Background.transform.position.x, main_Background.transform.position.y + bgHeight, 0);
+                OtherBackgroundSetting();
+                upCheck = 0;
+                downCheck = 0;
+            }
+        }
+    }
+
+    public void MainBackgroundDown()
+    {
+        if (downCheck >= (bgHeight / updownTileToTile) - 1)
+        {
+            if (Mathf.Abs(transform.position.y) % bgHeight < updownTileToTile)
+            {
+                main_Background.transform.position = new Vector3(main_Background.transform.position.x, main_Background.transform.position.y - bgHeight, 0);
+                OtherBackgroundSetting();
+                downCheck = 0;
+                upCheck = 0;
+            }
+        }
+    }
+    public void OtherBackgroundSetting()
+    {
+        other_Background[0].transform.position = new Vector3(main_Background.transform.position.x, main_Background.transform.position.y + bgHeight, 0);
+        other_Background[1].transform.position = new Vector3(main_Background.transform.position.x + bgWidth, main_Background.transform.position.y + bgHeight, 0);
+        other_Background[2].transform.position = new Vector3(main_Background.transform.position.x + bgWidth, main_Background.transform.position.y, 0);
+        other_Background[3].transform.position = new Vector3(main_Background.transform.position.x, main_Background.transform.position.y - bgHeight, 0);
+        other_Background[4].transform.position = new Vector3(main_Background.transform.position.x + bgWidth, main_Background.transform.position.y - bgHeight, 0);
     }
 }
